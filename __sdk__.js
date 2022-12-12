4f1b94c9add3108a1097ab6e31c7d9fff4bca6e0
ep-infosec <ep-infosec@users.noreply.github.com>
2022-12-12 20:00:53 +0200

    init

---
+++ __sdk__.js
@@ -0,0 +1,8 @@
+/* @flow */
+/* eslint import/no-commonjs: 0 */
+
+module.exports = {
+  legal: {
+    entry: "./src/index",
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+/* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable flowtype/require-valid-file-annotation */
+
+/* eslint import/no-nodejs-modules: off */
+
+import { getWebpackConfig } from "@krakenjs/webpack-config-grumbler";
+
+const FILE_NAME = "index";
+const MODULE_NAME = "paypal-legal-components";
+
+export const WEBPACK_CONFIG = getWebpackConfig({
+  filename: `${FILE_NAME}.js`,
+  modulename: MODULE_NAME,
+  minify: true,
+  vars: {
+    __MIN__: true,
+  },
+});
+export default [WEBPACK_CONFIG]; // eslint-disable-line import/no-default-export
